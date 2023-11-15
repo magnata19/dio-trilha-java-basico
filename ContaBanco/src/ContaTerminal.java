5b68e344c0220cf04bc0060fd4f68d25a9f9363c
Davidson <dehpacifico@gmail.com>
2023-11-14 21:55:05 -0300

    Desafio ContaTerminal  DIO concluido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -2,6 +2,24 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da sua conta. Ex: 1234");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência. Ex: 123");
+        int numeroAgencia = scanner.nextInt();
+        System.out.println("Digito: ");
+        int digitoAgencia = scanner.nextInt();
+
+        System.out.println("Por favor, nos informe seu nome.");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Nos informe seu saldo.");
+        double saldoCliente = scanner.nextDouble();
+
+        System.out.println(
+                "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia
+                        + "-" + digitoAgencia + ", conta " + numeroConta + " e seu saldo R$" + saldoCliente + " já está disponível para saque.");
     }
 }
